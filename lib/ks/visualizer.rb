c5871c964cd2a1f22630a60756702b7ef58d7de7
Mikhail Yakshin <greycat@altlinux.org>
2016-03-11 09:03:50 +0300

    Changed CLI arguments from "<format> <binary>" to "<binary> <formats>..." and implemented support for compilation and loading of multiple files

--- lib/ks/visualizer.rb
+++ lib/ks/visualizer.rb
@@ -8,10 +8,19 @@ require 'zlib'
 require 'stringio'
 
 class Visualizer
-  def initialize(format_fn, bin_fn)
-    @format_fn = format_fn
+  def initialize(bin_fn, formats_fn)
     @bin_fn = bin_fn
-    compile
+    @formats_fn = formats_fn
+    @primary_format = @formats_fn.shift
+
+    main_class_name = compile_format(@primary_format)
+
+    @formats_fn.each { |fn|
+      compile_format(fn)
+    }
+
+    main_class = Kernel::const_get(main_class_name)
+    @data = main_class.from_file(@bin_fn)
 
     @ui = TUI.new
     @tree = Tree.new(@ui, @data)
--- lib/ks/visualizer_main.rb
+++ lib/ks/visualizer_main.rb
@@ -1,9 +1,10 @@
 require 'ks/visualizer'
 
 class ExternalCompilerVisualizer < Visualizer
-  def compile
+  def compile_format(fn)
+    main_class_name = nil
     Dir.mktmpdir { |code_dir|
-      system("ksc -- -t ruby '#{@format_fn}' -d '#{code_dir}'")
+      system("ksc -- -t ruby '#{fn}' -d '#{code_dir}'")
       exit $?.exitstatus if $?.exitstatus != 0
 
       compiled_path = Dir.glob("#{code_dir}/*.rb")[0]
@@ -11,8 +12,8 @@ class ExternalCompilerVisualizer < Visualizer
       require compiled_path
 
       main_class_name = File.readlines(compiled_path).grep(/^class /)[0].strip.gsub(/^class /, '').gsub(/ <.*$/, '')
-      main_class = Kernel::const_get(main_class_name)
-      @data = main_class.from_file(@bin_fn)
     }
+
+    return main_class_name
   end
 end
